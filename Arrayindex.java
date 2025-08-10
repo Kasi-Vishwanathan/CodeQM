a3cb2303f9348e424b6057cea8e3859ee3d8cd2e
Kasi-Vishwanathan <coder0110wl@gmail.com>
2025-08-10 13:17:48 +0530

    Create Arrayindex.java

---
+++ Arrayindex.java
@@ -0,0 +1,8 @@
+public class ArrayBug {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        for(int i = 0; i <= arr.length; i++) {  // i <= arr.length causes out of bounds
+            System.out.println(arr[i]);
+        }
+    }
+}
